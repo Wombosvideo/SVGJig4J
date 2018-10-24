68da99259710e307e0044b58fbdd5af4126803a8
Wombosvideo <busfany@hotmail.com>
2018-10-24 14:14:01 +0200

    sync

--- src/ch/bosin/svgjig4j/Main.java
+++ src/ch/bosin/svgjig4j/Main.java
@@ -1,7 +1,10 @@
 package ch.bosin.svgjig4j;
 
+import org.la4j.LinearAlgebra;
 import org.la4j.Matrix;
 import org.la4j.Vector;
+import org.la4j.linear.GaussianSolver;
+import org.la4j.linear.LinearSystemSolver;
 import org.la4j.vector.SparseVector;
 
 import java.text.NumberFormat;
@@ -180,12 +183,8 @@ public class Main {
         Vector middle = middleRnd(startPoint, endPoint, randomizeBy);
         System.out.println(middle.mkString(NumberFormat.getNumberInstance(), ","));
 
-        Vector[] mid = middleEdgeRnd(startPoint, Vector.fromArray(new double[]{width, 0}), Vector.fromArray(new double[]{0, height}), endPoint, middle, randomizeBy);
-        Vector m1 = mid[0];
-        Vector m2 = mid[1];
+        middleEdgeRnd(startPoint, Vector.fromArray(new double[]{width, 0}), Vector.fromArray(new double[]{0, height}), endPoint, middle, randomizeBy);
 
-        System.out.println(m1.mkString(NumberFormat.getNumberInstance(), ","));
-        System.out.println(m2.mkString(NumberFormat.getNumberInstance(), ","));
 /*
         SparseVector startPoint = SparseVector.fromArray(new double[]{0,100});
         SparseVector endPoint = SparseVector.fromArray(new double[]{100,100});
@@ -205,31 +204,42 @@ public class Main {
 
     private static Vector middleRnd(Vector p1, Vector p2, double randomizeBy) {
         Vector r1 = p2.subtract(p1);
-        double d1 = r1.norm();
-        Vector e1 = r1.divide(d1);
-        double rnd = new Random().nextDouble();
-        double d1_ = d1/2 + rnd*randomizeBy*d1;
-        Vector out = e1.multiply(d1_).add(p1);
+        Vector tst1 = r1.divide(2);
+        Vector tst2 = tst1.add(p1);
+
+        Random random = new Random();
+        Vector mod = Vector.fromArray(new double[] {(random.nextBoolean() ? -1 : 1) * random.nextDouble() * randomizeBy * (p2.get(0) - p1.get(0)), (random.nextBoolean() ? -1 : 1) * random.nextDouble() * randomizeBy * (p2.get(1) - p1.get(1))});
+
+        Vector out = tst2.add(mod);
         return out;
     }
 
-    private static Vector[] middleEdgeRnd(Vector p1o1, Vector p2, Vector p3, Vector p4o2, Vector m1, double randomizeBy){
-        Vector r1 = p2.subtract(p1o1);
-        Vector r2 = p4o2.subtract(p3);
-        Vector r01 = m1.subtract(p1o1);
-        Vector r02 = p4o2.subtract(p1o1);
-        double d1 = r1.norm();
-        double d2 = r2.norm();
-        double d01 = r01.norm();
-        double d02 = r02.norm();
-        double d0 = d01/d02;
-        Vector e1 = r1.divide(d1);
-        Vector e2 = r2.divide(d2);
-        Vector wo1 = e1.multiply(d0);
-        Vector wo2 = e2.multiply(d0);
-        double rnd = new Random().nextDouble();
-        double d1_ = (((d1+d2)/2)/2 * d0) + rnd*randomizeBy*((d1+d2)/2);
-        Vector[] out = {e1.multiply(d1_).add(p1o1), e2.multiply(d1_).add(p3)};
-        return out;
+    private static Vector[] middleEdgeRnd(Vector p1, Vector p2, Vector p3, Vector p4, Vector m1, double randomizeBy){
+        Matrix inverse = Matrix.from1DArray(2,2,new double[]{0,1,-1,0});
+        Vector ab = p4.subtract(p3);double l_ab = ab.norm();Vector e_ab = ab.divide(l_ab),ne_ab = e_ab.multiply(inverse);
+        Vector bc = p2.subtract(p4);double l_bc = bc.norm();Vector e_bc = bc.divide(l_bc),ne_bc = e_bc.multiply(inverse);
+        Vector cd = p1.subtract(p2);double l_cd = cd.norm();Vector e_cd = cd.divide(l_cd),ne_cd = e_cd.multiply(inverse);
+        Vector da = p3.subtract(p1);double l_da = da.norm();Vector e_da = da.divide(l_da),ne_da = e_da.multiply(inverse);
+
+        Vector dm = m1.subtract(p1);double x_dm = dm.get(0), y_dm = dm.get(1);
+        Vector cm = m1.subtract(p2);double x_cm = -cm.get(0), y_cm = cm.get(1);
+        Vector am = m1.subtract(p3);double x_am = am.get(0), y_am = -am.get(1);
+
+        double  xa = p3.get(0), ya = p3.get(1),
+                xb = p4.get(0), yb = p4.get(1),
+                xc = p2.get(0), yc = p2.get(1),
+                xd = p1.get(0), yd = p1.get(1),
+                xm = m1.get(0), ym = m1.get(1);
+
+        double a1 = ((xc*(xd-xm)-Math.pow(xd,2)+xd*xm+(yc-yd)*(yd-ym))*Math.sqrt(Math.pow(xc,2)-2*xc*xd+Math.pow(xd,2)+Math.pow(yc-yd,2)))/(Math.pow(xc,2)-2*xc*xd+Math.pow(xd,2)+Math.pow(yc,2)-2*yc*yd+Math.pow(yd,2));
+
+        System.out.println(e_cd.multiply(a1).mkString(NumberFormat.getNumberInstance(), ","));
+
+        double dmc = 0.5 - x_dm/(x_dm + x_cm);
+        double dma = 0.5 - y_dm/(y_dm + y_am);
+
+        System.out.println("Relativ auf x: " + dmc + " [" + x_dm + ";" + y_dm + "], [" + x_cm + ";" + y_cm + "]");
+        System.out.println("Relativ auf y: " + dma + " [" + x_dm + ";" + y_dm + "], [" + x_am + ";" + y_am + "]");
+        return null;
     }
 }
