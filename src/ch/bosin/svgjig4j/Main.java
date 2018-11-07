02a0c83624645b981960ae23dc0a3f6f5bf0c71f
Wombosvideo <busfany@hotmail.com>
2018-11-07 19:40:55 +0100

    Synchronize to github
    Sorry I don't like descriptions

--- src/ch/bosin/svgjig4j/Main.java
+++ src/ch/bosin/svgjig4j/Main.java
@@ -1,184 +1,162 @@
 package ch.bosin.svgjig4j;
 
-import org.la4j.LinearAlgebra;
 import org.la4j.Matrix;
 import org.la4j.Vector;
-import org.la4j.linear.GaussianSolver;
-import org.la4j.linear.LinearSystemSolver;
 import org.la4j.vector.SparseVector;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.text.NumberFormat;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 import java.util.stream.Collectors;
 
 public class Main {
 
-    private final Vector startPoint, endPoint, unitVectorX, unitVectorY;
-
-    public Main(final Vector startPoint, final Vector endPoint) {
-        this.startPoint = startPoint;
-        this.endPoint = endPoint;
-        this.unitVectorX = this.endPoint.subtract(startPoint);
-        this.unitVectorY = this.unitVectorX.multiply(Matrix.from1DArray(2,2,new double[]{0,1,-1,0}));
-    }
-
-    private String endPointHelper(final Vector endPoint, final String letter) {
-        return letter + this.vecToString(endPoint);
-    }
-
-    /**
-     * Converts a vector into a string with a specified delimiter
-     * @param vector Vector to be converted into a string
-     * @param delimiter Delimiter as String
-     * @return a String with all coordinates delimited by the given delimiter
-     */
-    public String vecToString(final Vector vector, final String delimiter) {
-        return vector.mkString(NumberFormat.getNumberInstance(), delimiter);
-    }
-
-    /**
-     * Converts a vector into a string with "," as delimiter
-     * @param vector Vector to be converted into a string
-     * @return a String with all coordinates delimited by a ","
-     */
-    public String vecToString(final Vector vector){
-        return vector.mkString(NumberFormat.getNumberInstance(), ",");
-    }
-
-    /**
-     * Converts relative curve coordinates into an absolute point
-     * @param x Relative coordinate in direct direction
-     * @param y Relative coordinate in perpendicular direction
-     * @return absolute point as a Vector
-     */
-    public Vector curvePoint(final double x, final double y) {
-        return this.startPoint.add(this.unitVectorX.multiply(x).subtract(this.unitVectorY.multiply(y)));
-    }
-
-    /**
-     * Converts an absolute coordinate vector array to an svg curve
-     * @param curvePoints an absolute coordinate Vector array containing three vectors where the first vector is the control point of the
-     *                    start point, the second vector is the control point of the end point and the third vector is
-     *                    the end point itself.
-     * @return a String in the format "Cx1,y1 x2,y2 x,y"
-     */
-    public String curveTo(final Vector...curvePoints) {
-        return "C" + String.join(" ", Arrays.stream(curvePoints).map(this::vecToString).collect(Collectors.toList()));
-    }
-
-    /**
-     * Converts an absolute coordinate vector to an svg move to command
-     * @param endPoint an absolute coordinate Vector of the cursor point
-     * @return a String in the format "Mx,y"
-     */
-    public String moveTo(final Vector endPoint) {
-        return this.endPointHelper(endPoint, "M");
-    }
-
-    /**
-     * Moves the svg cursor using relative coordinates
-     * @param x Relative coordinates of cursor point in direct direction
-     * @param y Relative coordinates of cursor point in perpendicular direction
-     * @return a String in the format "Mx,y"
-     */
-    private String moveTo(final double x, final double y) {
-        return this.moveTo(this.curvePoint(x, y));
-    }
-
-    /**
-     * Creates an svg curve using relative coordinates
-     * @param x1 Relative coordinates of control point of start point in direct direction
-     * @param y1 Relative coordinates of control point of start point in perpendicular direction
-     * @param x2 Relative coordinates of control point of end point in direct direction
-     * @param y2 Relative coordinates of control point of end point in perpendicular direction
-     * @param x Relative coordinates of end point in direct direction
-     * @param y Relative coordinates of end point in perpendicular direction
-     * @return a String in the format "Cx1,y1 x2,y2 x,y"
-     */
-    private String curveTo(final double x1, final double y1, final double x2, final double y2, final double x, final double y) {
-        return this.curveTo(this.curvePoint(x1, y1), this.curvePoint(x2, y2), this.curvePoint(x, y));
-    }
-
-    /**
-     * Converts an absolute coordinate vector to an svg line
-     * @param endPoint an absolute coordinate Vector of the end point of the line
-     * @return a String in the format "Lx,y"
-     */
-    private String lineTo(final Vector endPoint) {
-        return this.endPointHelper(endPoint, "L");
-    }
-
-    /**
-     * Creates an svg line using relative coordinates
-     * @param x Relative coordinates of end point in direct direction
-     * @param y Relative coordinates of end point in perpendicular direction
-     * @return a String in the format "Lx,y"
-     */
-    private String lineTo(final double x, final double y) {
-        return this.lineTo(this.curvePoint(x, y));
-    }
-
-    public void run() {
+    public void run(SVGHelper svg) {
         // DEBUG: Prints start and end point and the unit vectors
-        System.out.println("A(" + this.vecToString(this.startPoint, "|") + ")");
-        System.out.println("B(" + this.vecToString(this.endPoint, "|") + ")");
-        System.out.println("u=[" + this.vecToString(this.unitVectorX, ";") + "]");
-        System.out.println("v=[" + this.vecToString(this.unitVectorY, ";") + "]");
+        System.out.println("A(" + svg.vecToString(svg.getStartPoint(), "|") + ")");
+        System.out.println("B(" + svg.vecToString(svg.getEndPoint(), "|") + ")");
+        System.out.println("u=[" + svg.vecToString(svg.getUnitVectorX(), ";") + "]");
+        System.out.println("v=[" + svg.vecToString(svg.getUnitVectorY(), ";") + "]");
 
         // Creates a new path
-        StringBuilder path = new StringBuilder();
+        svg.startPath();
 
         // Moves cursor to given start point (0|0)
-        path.append(this.moveTo(0.00, 0.00));
+        svg.moveTo(0.00, 0.00);
         // Draws a line to (0.3|0)
-        path.append(this.lineTo(0.40, 0.00));
+        svg.lineTo(0.40, 0.00);
 
         // Draws a curve to (0.23|0.2)
-        path.append(this.curveTo(
+        svg.curveTo(
                 0.475, 0.00,
                 0.39, 0.05,
                 0.365, 0.10
-        ));
+        );
         // Draws a curve to (0.33|0.4)
-        path.append(this.curveTo(
+        svg.curveTo(
                 0.345, 0.14,
                 0.375, 0.18,
                 0.415, 0.20
-        ));
+        );
         // Draws a curve to (0.67|0.4)
-        path.append(this.curveTo(
+        svg.curveTo(
                 0.465, 0.225,
                 0.535, 0.225,
                 0.585, 0.20
-        ));
+        );
         // Draws a curve to (0.77|0.2)
-        path.append(this.curveTo(
+        svg.curveTo(
                 0.625, 0.18,
                 0.655, 0.14,
                 0.635, 0.10
-        ));
+        );
         // Draws a curve to (0.7|0)
-        path.append(this.curveTo(
+        svg.curveTo(
                 0.61, 0.05,
                 0.525, 0.00,
                 0.60, 0.00
-        ));
+        );
 
         // Draws a line to given end point (1|0)
-        path.append(this.lineTo(endPoint));
+        svg.lineTo(svg.getEndPoint());
 
         // DEBUG: Prints out the d argument of the svg path
-        System.out.println(path.toString());
+        System.out.println(svg.getPath());
+
+        // Write to file
+        writeSVG(List.of(svg.getPathAndClear()), "end", svg.width, svg.height);
     }
 
     public static void main(String[] args) {
         int width = 1000;
         int height = 600;
+
+        int piecesX = 12; // aka. rows
+        int piecesY = 9; // aka. columns
+
         double randomizeBy = 0.1;
 
         SparseVector startPoint = SparseVector.fromArray(new double[] {0, 0});
         SparseVector endPoint = SparseVector.fromArray(new double[] {width, height});
+        SVGHelper svg = new SVGHelper(startPoint, endPoint);
+
+        Vector[][] storage = new Vector[piecesY+1][piecesX+1];
+        ArrayList<String> circles = new ArrayList<>();
+
+        for(int x = 0; x < piecesX-1; x++) {
+            double vx = Integer.valueOf(x).doubleValue() * (Integer.valueOf(width).doubleValue() / Integer.valueOf(piecesX-2).doubleValue());
+            storage[0][x] = Vector.fromArray(new double[] { vx, 0 });
+            storage[piecesY][x] = Vector.fromArray(new double[] { vx, height });
+            circles.add("<circle cx=\"" + vx + "\" cy=\"" + 0 + "\" r=\"" + (((width/(piecesX-1) + height/(piecesY-1))/2)/10) + "\"/>\n");
+            circles.add("<circle cx=\"" + vx + "\" cy=\"" + height + "\" r=\"" + (((width/(piecesX-1) + height/(piecesY-1))/2)/10) + "\"/>\n");
+        }
+/*        for(int y = 1; y < piecesY; y++){
+            double vy = Integer.valueOf(y).doubleValue() * (Integer.valueOf(height).doubleValue() / Integer.valueOf(piecesY).doubleValue());
+            storage[y][0] = Vector.fromArray(new double[] { 0, vy });
+            storage[y][piecesX] = Vector.fromArray(new double[] { width, vy });
+            circles.add("<circle cx=\"" + 0 + "\" cy=\"" + vy + "\" r=\"" + (((width/piecesX + height/piecesY)/2)/10) + "\"/>\n");
+            circles.add("<circle cx=\"" + width + "\" cy=\"" + vy + "\" r=\"" + (((width/piecesX + height/piecesY)/2)/10) + "\"/>\n");
+        }*/
+
+        System.out.println(Arrays.deepToString(storage));
+
+        // Write to output-stg_01.svg
+        writeSVG(circles, "stg_01", width, height);
+
+        int randomIndexX = 8;
+        circles.set(2 * randomIndexX, circles.get(2 * randomIndexX).replaceAll("\\/>", " class=\"b\"\\/>"));
+        circles.set(2 * randomIndexX + 1, circles.get(2 * randomIndexX + 1).replaceAll("\\/>", " class=\"b\"\\/>"));
+
+        writeSVG(circles, "stg_02", width, height);
+
+        Vector posTop = storage[0][randomIndexX];
+        Vector posBottom = storage[piecesY][randomIndexX];
+
+        svg.startPath();
+        svg.moveTo(posTop);
+        svg.lineTo(posBottom);
+        circles.add(svg.getPathAndClear());
+
+        writeSVG(circles, "stg_03", width, height);
+
+        int piecesY_ = 1;
+
+        Vector posTemp, posMiddle = posTemp = middleRnd(posTop, posBottom, randomizeBy, Integer.valueOf(Math.floorDiv(piecesY, 2)).doubleValue()/Integer.valueOf(piecesY).doubleValue());
+        circles.add("<circle cx=\"" + posMiddle.get(0) + "\" cy=\"" + posMiddle.get(1) + "\" r=\"" + (((width/piecesX + height/piecesY)/2)/10) + "\" class=\"b\"/>\n");
+        piecesY_++;
+
+
+/*
+        posMiddle = middleRnd(posTop, posTemp, randomizeBy, 1D/6D);
+        circles.add("<circle cx=\"" + posMiddle.get(0) + "\" cy=\"" + posMiddle.get(1) + "\" r=\"" + (((width/piecesX + height/piecesY)/2)/10) + "\" class=\"b\"/>\n");
+        piecesY_++;
+        posMiddle = middleRnd(posTop, posTemp, randomizeBy, 2D/3D);
+        circles.add("<circle cx=\"" + posMiddle.get(0) + "\" cy=\"" + posMiddle.get(1) + "\" r=\"" + (((width/piecesX + height/piecesY)/2)/10) + "\" class=\"b\"/>\n");
+        piecesY_++;
+
+        posMiddle = middleRnd(posTemp, posBottom, randomizeBy, 1D/3D);
+        circles.add("<circle cx=\"" + posMiddle.get(0) + "\" cy=\"" + posMiddle.get(1) + "\" r=\"" + (((width/piecesX + height/piecesY)/2)/10) + "\" class=\"b\"/>\n");
+        piecesY_++;
+        posMiddle = middleRnd(posTemp, posBottom, randomizeBy, 2D/3D);
+        circles.add("<circle cx=\"" + posMiddle.get(0) + "\" cy=\"" + posMiddle.get(1) + "\" r=\"" + (((width/piecesX + height/piecesY)/2)/10) + "\" class=\"b\"/>\n");
+        piecesY_++;
+*/
+
+        writeSVG(circles, "stg_04", width, height);
+
+        ArrayList<Vector> test = middleCntd(posTop, posBottom, randomizeBy, piecesY-1);
+        System.out.println("What the heck: " + Arrays.toString(test.toArray()));
+        for(Vector v : test) {
+            circles.add("<circle cx=\"" + v.get(0) + "\" cy=\"" + v.get(1) + "\" r=\"" + (((width/(piecesX-1) + height/(piecesY-1))/2)/12) + "\" class=\"c\"/>\n");
+        }
+
+        writeSVG(circles, "stg_05", width, height);
+
 
         Vector middle = middleRnd(startPoint, endPoint, randomizeBy);
         System.out.println(middle.mkString(NumberFormat.getNumberInstance(), ","));
@@ -202,16 +180,104 @@ public class Main {
 */
     }
 
+    private static void writeSVG(List<String> paths, String releaseName, int width, int height) {
+        // Write to file
+        String svgData = "<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 " + width + " " + height + "\">\n" +
+                "  <defs>\n" +
+                "    <style>\n" +
+                "      .a {\n" +
+                "        fill: none;\n" +
+                "        stroke: #e52421;\n" +
+                "        stroke-miterlimit: 10;\n" +
+                "      }\n" +
+                "      .b {\n" +
+                "        fill: #e52421;\n" +
+                "      }\n" +
+                "      .c {\n" +
+                "        fill: #0000ff;\n" +
+                "      }\n" +
+                "      .d {\n" +
+                "        fill: #00ff00;\n" +
+                "      }\n" +
+                "    </style>\n" +
+                "  </defs>\n" +
+                "  <title>curve_1</title>\n";
+        for(String path : paths)
+            svgData += "  " + path;
+        svgData +=
+                "</svg>\n";
+        try{
+            System.out.println("Writing file");
+            Files.write(Paths.get("G:\\Matura\\output-" + releaseName + ".svg"), svgData.getBytes());
+        } catch(IOException e) {
+            System.err.println(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    private static ArrayList<Vector> middleCntd(Vector p1, Vector p2, double randomizeBy, int frac) {
+        return middleCntd(p1, p2, randomizeBy, frac, "c");
+    }
+
+    private static ArrayList<Vector> middleCntd(Vector p1, Vector p2, double randomizeBy, int frac, String klass) {
+        int fracLow = Math.floorDiv(frac, 2);
+        int fracHigh = frac-fracLow;
+        ArrayList<Vector> vectors = new ArrayList<>();
+
+        Vector m = middleRnd(p1, p2, randomizeBy, Integer.valueOf(fracLow).doubleValue()/Integer.valueOf(frac).doubleValue());
+
+        if(fracLow == 1) {
+            // Don't know what to do
+            System.out.println("Don't know what to do: " + fracLow + "/" + frac + " or " + fracHigh + "/" + frac);
+            vectors.add(middleRnd(p1, m, randomizeBy, Integer.valueOf(fracLow).doubleValue()/Integer.valueOf(frac).doubleValue()));
+        }
+        if(fracHigh == 1) {
+            // Don't know what to do
+            System.out.println("Don't know what to do: " + fracLow + "/" + frac + " or " + fracHigh + "/" + frac);
+            vectors.add(middleRnd(m, p2, randomizeBy, Integer.valueOf(fracLow).doubleValue()/Integer.valueOf(frac).doubleValue()));
+        }
+        if(fracHigh == 1 || fracLow == 1)
+            return vectors;
+
+        if(fracLow%2==0) {
+            System.out.println("This is low funny: " + fracLow);
+            vectors.addAll(middleCntd(p1, m, randomizeBy, fracLow));
+        } else {
+            for(int i = 1; i < fracLow; i++){
+                Vector mdl = middleRnd(p1, m, randomizeBy, Integer.valueOf(i).doubleValue()/Integer.valueOf(fracLow).doubleValue());
+                vectors.add(mdl);
+            }
+        }
+
+        if(fracHigh%2==0) {
+            System.out.println("This is high funny: " + fracHigh);
+            vectors.addAll(middleCntd(m, p2, randomizeBy, fracHigh));
+        } else {
+            for(int i = 1; i < fracHigh; i++){
+                Vector mdl = middleRnd(m, p2, randomizeBy, Integer.valueOf(i).doubleValue()/Integer.valueOf(fracHigh).doubleValue());
+                vectors.add(mdl);
+            }
+        }
+        return vectors;
+    }
+
     private static Vector middleRnd(Vector p1, Vector p2, double randomizeBy) {
+        return middleRnd(p1, p2, randomizeBy, 0.5D);
+    }
+
+    private static Vector middleRnd(Vector p1, Vector p2, double randomizeBy, double frac) {
         Vector r1 = p2.subtract(p1);
-        Vector tst1 = r1.divide(2);
+        Vector tst1 = r1.multiply(frac);
         Vector tst2 = tst1.add(p1);
 
         Random random = new Random();
-        Vector mod = Vector.fromArray(new double[] {(random.nextBoolean() ? -1 : 1) * random.nextDouble() * randomizeBy * (p2.get(0) - p1.get(0)), (random.nextBoolean() ? -1 : 1) * random.nextDouble() * randomizeBy * (p2.get(1) - p1.get(1))});
+        System.out.println("DEBUG: x" + (random.nextBoolean() ? 0.5 : -0.5)*randomizeBy*random.nextDouble()*r1.get(0));
+        System.out.println("DEBUG: y" + (random.nextBoolean() ? 0.5 : -0.5)*randomizeBy*random.nextDouble()*r1.get(1));
+        System.out.println(tst2.mkString(NumberFormat.getNumberInstance(), ","));
+        Vector mod = Vector.fromArray(new double[] {tst2.get(0) + (random.nextBoolean() ? 0.5 : -0.5)*randomizeBy*random.nextDouble()*r1.get(0), tst2.get(1) + (random.nextBoolean() ? 0.5 : -0.5)*randomizeBy*random.nextDouble()*r1.get(1) });
 
         Vector out = tst2.add(mod);
-        return out;
+        return mod;
     }
 
     private static Vector[] middleEdgeRnd(Vector p1, Vector p2, Vector p3, Vector p4, Vector m1, double randomizeBy){
--- src/ch/bosin/svgjig4j/SVGHelper.java
+++ src/ch/bosin/svgjig4j/SVGHelper.java
@@ -1,4 +1,157 @@
 package ch.bosin.svgjig4j;
 
+import org.la4j.Matrix;
+import org.la4j.Vector;
+
+import java.text.NumberFormat;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class SVGHelper {
+
+    private final Vector startPoint, endPoint, unitVectorX, unitVectorY;
+    private StringBuilder path;
+    public final int width, height;
+
+    public SVGHelper(final Vector startPoint, final Vector endPoint) {
+        this.startPoint = startPoint;
+        this.endPoint = endPoint;
+        this.unitVectorX = this.endPoint.subtract(startPoint);
+        this.unitVectorY = this.unitVectorX.multiply(Matrix.from1DArray(2,2,new double[]{0,1,-1,0}));
+        this.width = Double.valueOf(Math.ceil(this.endPoint.subtract(this.startPoint).get(0))).intValue();
+        this.height = Double.valueOf(Math.ceil(this.endPoint.subtract(this.startPoint).get(1))).intValue();
+    }
+
+    private String endPointHelper(final Vector endPoint, final String letter) {
+        return letter + this.vecToString(endPoint);
+    }
+
+    /**
+     * Converts a vector into a string with a specified delimiter
+     * @param vector Vector to be converted into a string
+     * @param delimiter Delimiter as String
+     * @return a String with all coordinates delimited by the given delimiter
+     */
+    public String vecToString(final Vector vector, final String delimiter) {
+        return vector.mkString(NumberFormat.getNumberInstance(), delimiter);
+    }
+
+    /**
+     * Converts a vector into a string with "," as delimiter
+     * @param vector Vector to be converted into a string
+     * @return a String with all coordinates delimited by a ","
+     */
+    public String vecToString(final Vector vector){
+        return vector.mkString(NumberFormat.getNumberInstance(), ",");
+    }
+
+    /**
+     * Converts relative curve coordinates into an absolute point
+     * @param x Relative coordinate in direct direction
+     * @param y Relative coordinate in perpendicular direction
+     * @return absolute point as a Vector
+     */
+    public Vector curvePoint(final double x, final double y) {
+        return this.startPoint.add(this.unitVectorX.multiply(x).subtract(this.unitVectorY.multiply(y)));
+    }
+
+    /**
+     * Converts an absolute coordinate vector array to an svg curve
+     * @param curvePoints an absolute coordinate Vector array containing three vectors where the first vector is the control point of the
+     *                    start point, the second vector is the control point of the end point and the third vector is
+     *                    the end point itself.
+     * @return a String in the format "Cx1,y1 x2,y2 x,y"
+     */
+    public String curveTo(final Vector...curvePoints) {
+        if(this.path != null)
+            path.append("C" + String.join(" ", Arrays.stream(curvePoints).map(this::vecToString).collect(Collectors.toList())));
+        return "C" + String.join(" ", Arrays.stream(curvePoints).map(this::vecToString).collect(Collectors.toList()));
+    }
+
+    /**
+     * Converts an absolute coordinate vector to an svg move to command
+     * @param endPoint an absolute coordinate Vector of the cursor point
+     * @return a String in the format "Mx,y"
+     */
+    public String moveTo(final Vector endPoint) {
+        if(this.path != null)
+            path.append(this.endPointHelper(endPoint, "M"));
+        return this.endPointHelper(endPoint, "M");
+    }
+
+    /**
+     * Moves the svg cursor using relative coordinates
+     * @param x Relative coordinates of cursor point in direct direction
+     * @param y Relative coordinates of cursor point in perpendicular direction
+     * @return a String in the format "Mx,y"
+     */
+    public String moveTo(final double x, final double y) {
+        return this.moveTo(this.curvePoint(x, y));
+    }
+
+    /**
+     * Creates an svg curve using relative coordinates
+     * @param x1 Relative coordinates of control point of start point in direct direction
+     * @param y1 Relative coordinates of control point of start point in perpendicular direction
+     * @param x2 Relative coordinates of control point of end point in direct direction
+     * @param y2 Relative coordinates of control point of end point in perpendicular direction
+     * @param x Relative coordinates of end point in direct direction
+     * @param y Relative coordinates of end point in perpendicular direction
+     * @return a String in the format "Cx1,y1 x2,y2 x,y"
+     */
+    public String curveTo(final double x1, final double y1, final double x2, final double y2, final double x, final double y) {
+        return this.curveTo(this.curvePoint(x1, y1), this.curvePoint(x2, y2), this.curvePoint(x, y));
+    }
+
+    /**
+     * Converts an absolute coordinate vector to an svg line
+     * @param endPoint an absolute coordinate Vector of the end point of the line
+     * @return a String in the format "Lx,y"
+     */
+    public String lineTo(final Vector endPoint) {
+        if(this.path != null)
+            path.append(this.endPointHelper(endPoint, "L"));
+        return this.endPointHelper(endPoint, "L");
+    }
+
+    /**
+     * Creates an svg line using relative coordinates
+     * @param x Relative coordinates of end point in direct direction
+     * @param y Relative coordinates of end point in perpendicular direction
+     * @return a String in the format "Lx,y"
+     */
+    public String lineTo(final double x, final double y) {
+        return this.lineTo(this.curvePoint(x, y));
+    }
+
+    public void startPath() {
+        this.path = new StringBuilder();
+    }
+
+    public String getPath() {
+        return "<path class=\"a\" d=\"" + this.path.toString() + "\"/>\n";
+    }
+
+    public String getPathAndClear() {
+        String path = this.getPath();
+        this.path.delete(0, this.path.length());
+        this.path = null;
+        return path;
+    }
+
+    public Vector getEndPoint() {
+        return endPoint;
+    }
+
+    public Vector getStartPoint() {
+        return startPoint;
+    }
+
+    public Vector getUnitVectorX() {
+        return unitVectorX;
+    }
+
+    public Vector getUnitVectorY() {
+        return unitVectorY;
+    }
 }
